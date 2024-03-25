5bc23b45624fb06dcd8c7c6205fe541d5e3db9f3
tayyba <95464545+TayybaSaveera@users.noreply.github.com>
2024-03-26 00:17:30 +0500

    updated

--- test_flask.py
+++ test_flask.py
@@ -2,17 +2,13 @@
 import pytest
 from app import app
 
-
 @pytest.fixture
 def client():
     with app.test_client() as client:
         yield client
-
 
 def test_predict_route(client):
     data = {'features': [5.1, 3.5, 1.4, 0.2]}
     response = client.post('/predict', json=data)
     assert response.status_code == 200
     assert 'prediction' in response.json()
-
-
