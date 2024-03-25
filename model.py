a607df29fe599da1235307d18468b69c6e9329bf
tayyba <95464545+TayybaSaveera@users.noreply.github.com>
2024-03-26 00:11:30 +0500

    updated

--- model.py
+++ model.py
@@ -9,7 +9,9 @@ X = iris.data
 y = iris.target
 
 # Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
 
 # Train a logistic regression model
 model = LogisticRegression()
