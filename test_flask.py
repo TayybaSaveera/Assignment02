7f766d964c11bf84312206bb9b5522a580cfb96d
tayyba <95464545+TayybaSaveera@users.noreply.github.com>
2024-03-26 00:19:34 +0500

    updated

--- test_flask.py
+++ test_flask.py
@@ -2,10 +2,12 @@
 import pytest
 from app import app
 
+
 @pytest.fixture
 def client():
     with app.test_client() as client:
         yield client
+
 
 def test_predict_route(client):
     data = {'features': [5.1, 3.5, 1.4, 0.2]}
